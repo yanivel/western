7fd5a2c2a203421d9d8d88bc8f6c1bd883f6e6d7
yanivel <yaniv.elimor@gmail.com>
2015-02-16 16:09:55 +0200

    Asd

--- main.js
+++ main.js
@@ -4,19 +4,31 @@ window.onload = function () {
 			yElem = document.getElementById('yCoord'),
 			zElem = document.getElementById('zCoord');
 
+		var x,
+			y,
+			z;
+
 		var deviceMotionHandler = function(e) 
 		{
 			var acc = e.acceleration;
 
-			xElem.innerHTML = acc.x;
-			yElem.innerHTML = acc.y;
-			zElem.innerHTML = acc.z;
+			x = acc.x;
+			y = acc.y;
+			z = acc.z;
+		}
+
+		var updateElements() {
+			xElem.innerHTML = x;
+			yElem.innerHTML = y;
+			zElem.innerHTML = z;
 		}
 
 		var successElt = document.getElementById('success');
 		successElt.className = "alert alert-success";
 		successElt.innerHTML = "DeviceMotionEvent is supported!";
 		window.addEventListener('devicemotion', deviceMotionHandler, false);
+
+		setInterval(updateElements, 500);
 	} else {
 		var errorElt = document.getElementById("error");
 		errorElt.innerHTML = "DeviceMotionEvent not supported!";
