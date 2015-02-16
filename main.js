3419d8e139f3ca0b2f197ad88bbaa324efcb2ae6
yanivel <yaniv.elimor@gmail.com>
2015-02-16 16:03:46 +0200

    Asd

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
+window.onload = function () {
+	if (window.DeviceMotionEvent) {
+		var xElem = document.getElementById('xCoord'),
+			yElem = document.getElementById('yCoord'),
+			zElem = document.getElementById('zCoord');
 
-if (window.DeviceMotionEvent) {
-	var xElem = document.getElementById('xCoord'),
-		yElem = document.getElementById('yCoord'),
-		zElem = document.getElementById('zCoord');
+		var deviceMotionHandler = function(e) 
+		{
+			var acc = e.acceleration;
 
-	var deviceMotionHandler = function(e) 
-	{
-		var acc = e.acceleration;
+			xElem.innerHTML = acc.x;
+			yElem.innerHTML = acc.y;
+			zElem.innerHTML = acc.z;
+		}
 
-		xElem.innerHTML = acc.x;
-		yElem.innerHTML = acc.y;
-		zElem.innerHTML = acc.z;
+		var successElt = document.getElementById('success');
+		successElt.className = "alert alert-success";
+		successElt.innerHTML = "DeviceMotionEvent is supported!";
+		window.addEventListener('devicemotion', deviceMotionHandler, false);
+	} else {
+		var errorElt = document.getElementById("error");
+		errorElt.innerHTML = "DeviceMotionEvent not supported!";
+		errorElt.className = "alert alert-danger";
 	}
-
-	window.getElementById("success").innerHTML = "DeviceMotionEvent is supported!";
-	window.addEventListener('devicemotion', deviceMotionHandler, false);
-} else {
-	window.getElementById("error").innerHTML = "DeviceMotionEvent not supported!";
 }
