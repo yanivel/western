ded33904eaa636e5e532384fd229028097e5d49d
yanivel <yaniv.elimor@gmail.com>
2015-02-16 16:48:37 +0200

    Asd

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 window.onload = function () {
+
+	var width = 300;
+	var height = 300;
+
+	var centerX = 300/2;
+	var centerY = 300/2;
+
+	var el = document.getElementById("vector"),
+    two = new Two({ 
+        width: width,
+        height: height
+    });
+ 
+	two.appendTo(el);
+
+
+
 	if (window.DeviceMotionEvent) {
 		var xElem = document.getElementById('xCoord'),
 			yElem = document.getElementById('yCoord'),
@@ -21,6 +38,12 @@ window.onload = function () {
 			xElem.innerHTML = x;
 			yElem.innerHTML = y;
 			zElem.innerHTML = z;
+
+			var line = two.makeLine(centerX, centerY, centerX + x, centerY + y);
+			line.linewidth = 10;
+			line.stroke = "rgba(255, 0, 0, 0.5)";
+
+			two.update();
 		}
 
 		var successElt = document.getElementById('success');
