ac33d35c4e0b534755aba25f8e7db0da6caa47c7
yanivel <yaniv.elimor@gmail.com>
2015-02-16 15:40:38 +0200

    Asd

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var xElem = document.getElementById('xCoord'),
 	yElem = document.getElementById('yCoord'),
 	zElem = document.getElementById('zCoord');
 
-function deviceMotionHandler(e) 
+ var = deviceMotionHandler = function(e) 
 {
 	var acc = e.acceleration;
 
